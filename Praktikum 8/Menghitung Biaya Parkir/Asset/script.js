2a65a482382f9dd330c1284f05be77e11d4db153
HijriAzamAsqolany <92269571+HijriAzamAsqolany@users.noreply.github.com>
2022-01-03 23:19:51 +0700

    Add files via upload

---
+++ Praktikum 8/Menghitung Biaya Parkir/Asset/script.js
@@ -0,0 +1,17 @@
+function hitungParkir(){
+    let jam_masuk = document.getElementById("masuk").value;
+    let jam_keluar = document.getElementById("keluar").value;
+    let selisih = jam_keluar - jam_masuk;
+    let biaya = 0;
+    const TARIF_AWAL = 3000;
+    const TARIF_PERJAM = 1000;// perjam berikutnya
+    if(selisih <= 2) {// yang kurang dari atau sama dengan 2
+        biaya = TARIF_AWAL;
+    }
+    else {
+        let tambahan_jam = selisih - 2;
+        biaya = TARIF_AWAL + (TARIF_PERJAM * tambahan_jam);
+    }
+    let area_biaya = document.getElementById("biaya");
+    area_biaya.innerHTML = biaya;
+}
